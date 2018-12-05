f159131e17e13bad283eb441fedeabdfadc4a0bf
Toni Stapleton <toni.stapleton@iriworldwide.com>
2018-12-05 14:14:11 -0600

    Add updated PyPoll code and Jupyter Notebook

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,6 +2,7 @@ import os
 import csv
 csvpath = os.path.join('Desktop','PythonStuff', 'python-challenge', 'PyPoll','election_data.csv')
 num_voters=0
+candidate=[]
 with open(csvpath, newline='') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
@@ -10,7 +11,14 @@ with open(csvpath, newline='') as csvfile:
     
     numbers=list(csvreader)
     num_voters = len(numbers)
-
+    
+    for row in csvreader:
+        if row[2] not in candidate:  
+            print(row[2])
+            candidate.append(row[2])
+     
+print(candidate)
 print(f"Election Results")
 print(f"----------------------------")
 print(f"Total Votes: {num_voters}")
+print(f"----------------------------")
